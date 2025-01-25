a8a8c90692d9145b084a99e3d4c5339c93c09eb1
Satyam Stellar <satyam321tomar@gmail.com>
2025-01-25 20:47:32 +0530

    Update patterns

--- Patterns/cpp.cpp
+++ Patterns/cpp.cpp
@@ -2,84 +2,128 @@
 
 using namespace std;
 
-void print1(int n) {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
+void print1(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
       cout << "* ";
     }
     cout << endl;
   }
 }
 
-void print2(int n) {
-  for (int i = 0; i <= n; i++) {
-    for (int j = 0; j <= i; j++) {
+void print2(int n)
+{
+  for (int i = 0; i <= n; i++)
+  {
+    for (int j = 0; j <= i; j++)
+    {
       cout << "* ";
     }
     cout << endl;
   }
 }
 
-void print3(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= i; j++) {
+void print3(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
       cout << j << " ";
     }
     cout << endl;
   }
 }
 
-void print4(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= i; j++) {
+void print4(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
       cout << i << " ";
     }
     cout << endl;
   }
 }
 
-void print5(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n - i + 1; j++) {
+void print5(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n - i + 1; j++)
+    {
       cout << j << " ";
     }
     cout << endl;
   }
 }
 
-void print6(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n - i + 1; j++) {
+void print6(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n - i + 1; j++)
+    {
       cout << "* ";
     }
     cout << endl;
   }
 }
 
-void print7(int n) {
-  for (int i = 0; i <= n; i++) {
-    for (int j = 0; j <= n - i - 1; j++) {
+void print7(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
       cout << " ";
     }
-    for (int j = 0; j <= 2 * i + 1; j++) {
+    for (int j = 0; j < 2 * i + 1; j++)
+    {
       cout << "*";
     }
-    for (int j = 0; j <= n - i - 1; j++) {
+    for (int j = 0; j < n - i - 1; j++)
+    {
       cout << " ";
     }
     cout << endl;
   }
 }
 
-int main() {
-  int t;
-  cin >> t;
-  for (int i = 0; i <= t; i++) {
-    int n;
-    cin >> n;
-    print7(n);
-
+void print8(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j < n * i; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 0; j < i; j++)
+    {
+      cout << " ";
+    }
     cout << endl;
   }
 }
 
+int main()
+{
+  int t;
+  cin >> t;
+  for (int i = 0; i <= t; i++)
+  {
+    int n;
+    cin >> n;
+    print8(n);
+
+    cout << endl;
+  }
+}
